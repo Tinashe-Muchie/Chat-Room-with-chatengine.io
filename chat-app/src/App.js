c4045b00656631766751baef1e3908dd117c190b
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-03-16 20:49:43 +0200

    appjs modifications

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -2,20 +2,27 @@ import React from 'react'
 import {ChatEngine} from 'react-chat-engine'
 import './App.css'
 import Login from './components/Login/Login'
+import ChatArea from './components/ChatArea/ChatArea'
 
 function App() {
+
   return (
     <div>
-    <Login />
-    {/*<ChatEngine 
-      publicKey={'db666265-557a-44c8-92a1-f9261e58cc4e'}
-      userName={'Tinashe'}
-      userSecret={'Takudzwa@16'}
-      height='100vh'
-     // hideUI={true}
-    />*/} 
-    </div>
-      
+    {
+      (!localStorage.getItem('user'))
+      ? <Login />
+      : <>
+          <ChatEngine 
+            publicKey={'db666265-557a-44c8-92a1-f9261e58cc4e'}
+            userName={localStorage.getItem('user')}
+            userSecret={localStorage.getItem('password')}
+            height='100vh'
+            hideUI={true}
+          />
+          <ChatArea />
+        </>  
+    } 
+    </div>    
   );
 }
 
