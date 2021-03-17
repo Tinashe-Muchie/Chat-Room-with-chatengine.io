c5c2c6214c2d1708c8794c8b6a8b57ab13b457fc
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-03-17 19:11:04 +0200

    chats modal modifications

--- chat-app/src/components/Modals/ChatsModal.js
+++ chat-app/src/components/Modals/ChatsModal.js
@@ -1,13 +1,35 @@
-import React from 'react'
-import {Modal, Form, Button} from 'react-bootstrap'
+import React, {useState} from 'react'
+import {Modal, Form, Button, Alert} from 'react-bootstrap'
+import axios from 'axios'
+
+const projectID = 'db666265-557a-44c8-92a1-f9261e58cc4e'
 
 function ChatsModal({handleClose}) {
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const [title, setTitle] = useState('')
+    const [error, setError] = useState('')
 
-        handleClose()
+    const authObject = {
+        'Project-ID': projectID,
+        'User-Name': localStorage.getItem('user'),
+        'User-Secret': localStorage.getItem('password')
     }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            axios.post(
+                'https://api.chatengine.io/chats/',
+                { 'title': title, 'admin_username': localStorage.getItem('user') },
+                { headers: authObject },    
+            )
+            handleClose()
+        }catch(error){
+            setError('Oops! something went wrong, try again.')
+        }   
+    }
+
+    const err = (error) ? true : false
 
     return (
            <div className="modal-wrapper">
@@ -17,11 +39,21 @@ function ChatsModal({handleClose}) {
             <Modal.Body>
                 <Form onSubmit={handleSubmit}>
                         <Form.Group>
-                            <Form.Control type="text" placeholder="Enter Chat Title..." required />
+                            <Form.Control 
+                                type="text" 
+                                placeholder="Enter Chat Title..." 
+                                value={title}
+                                onChange={(e)=>setTitle(e.target.value)} 
+                                required />
                         </Form.Group>
                     <Button type="submit" variant="outline-primary" className="mb-3 float-right">
                         Create
                     </Button>
+                    <div className="mt-3" style={{display:'flex', justifyContent:'center'}}>
+                        {(err) && <Alert variant='dark' style={{color: '#C5C6C7', backgroundColor: '#3500D3'}}>
+                            {error}
+                        </Alert>}
+                    </div>
                 </Form>
             </Modal.Body> 
         </div> 
