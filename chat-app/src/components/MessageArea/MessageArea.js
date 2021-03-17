d0a89fcce3ab540e7853c8494c85d6f7f21a978c
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-03-17 19:10:51 +0200

    modifications message area

--- chat-app/src/components/MessageArea/MessageArea.js
+++ chat-app/src/components/MessageArea/MessageArea.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {Button, Form, InputGroup } from 'react-bootstrap'
+import {Button, Form, InputGroup} from 'react-bootstrap'
 
 function MessageArea() {
 
@@ -9,13 +9,31 @@ function MessageArea() {
         e.preventDefault()
 
     }
+
+    const handleLogout = ()=> {
+        localStorage.removeItem('user')
+        localStorage.removeItem('password')
+
+        window.location.reload()
+    }
+
     return (
         <div className="d-flex flex-column flex-grow-1 ml-1">
-            <Button variant="outline-primary" type="submit" className="float-right mb-1 mt-2" size="small">
+            <Button 
+                variant="outline-primary" 
+                type="button" 
+                className="float-right mb-1 mt-2" 
+                size="small" 
+                onClick={handleLogout}>
                 Logout
             </Button>
+            <div className="id-tab mb-1 text-small text-muted">
+                    ChatTitle: <br/>
+                    Participants:
+            </div>
             <div className="tab-content flex-grow-1 overflow-auto mb-1">
                 <div className="d-flex flex-column align-items-end justify-content-end px-1 py-2">
+
                 </div>
             </div>
             <Form onSubmit={handleSubmit}>
