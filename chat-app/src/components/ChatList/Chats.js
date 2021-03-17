c1a6ee123ca0056ca60046a6d2503145fa164a0f
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-03-17 19:10:11 +0200

    chats modifications

--- chat-app/src/components/ChatList/Chats.js
+++ chat-app/src/components/ChatList/Chats.js
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {ListGroup} from 'react-bootstrap'
 
 function Chats({chats}) {
+
+    const [activeChat, setActiveChat] = useState(0)
+    let selectedChat = chats.map((chat)=>{
+            return (chat.id === activeChat)
+        })
+
+    if(chats)
     return (
         <div>
             <ListGroup variant="flush">
-                {chats && chats.map((chat, index)=>(
-                    <ListGroup.Item 
-                        key={index} 
-                        action
-                        active
-                        className="text-left"
-                    >
-                        {chat.title}
-                    </ListGroup.Item>
-                ))}
+                {
+                    chats.length > 0
+                    ?   chats.map((chat, index)=>(
+                        <ListGroup.Item 
+                            key={chat.id} 
+                            action
+                            active={selectedChat[index]}
+                            onClick={()=>setActiveChat(chat.id)}
+                            className="text-left"
+                        >
+                            {chat.title}
+                        </ListGroup.Item>
+                    ))
+                    :   <div style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            marginTop: '25vh',
+                            fontWeight: 'bolder'
+                        }}>
+                            Create New Chats!
+                        </div>
+            }
             </ListGroup>
         </div>
     )
