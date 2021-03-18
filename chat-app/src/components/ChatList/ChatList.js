2579f51dbe4b4a77c74411d7fa0ef732940b93c9
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-03-18 16:12:52 +0200

    chatlist modifications

--- chat-app/src/components/ChatList/ChatList.js
+++ chat-app/src/components/ChatList/ChatList.js
@@ -1,60 +1,28 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useContext} from 'react'
 import {Tab, Nav, Button, Modal} from 'react-bootstrap'
 import ChatsModal from '../Modals/ChatsModal'
 import ContactsModal from '../Modals/ContactsModal'
-import axios from 'axios'
 import Chats from './Chats'
 import Contacts from  './Contacts'
-
-const projectID = 'db666265-557a-44c8-92a1-f9261e58cc4e'
-const projectKey = '6346e83a-eb04-49bf-899b-b0850ef36b5a'
+import {Context} from '../Context/Context'
 
 function ChatList() {
 
     const chatsKey = 'chats'
     const contactsKey = 'contacts'
+    const {chats, contacts} = useContext(Context)
 
     const [key, setKey] = useState(chatsKey)
     const [show, setShow] =useState(false)
-    const [chats, setChats] = useState([])
-    const [contacts, setContacts] = useState([])
 
     const chatsOpen = key === chatsKey
     const handleOpen = ()=> setShow(true)
     const handleClose = ()=> setShow(false)
-
-    useEffect(()=>{
-        const authObject = {
-            'Project-ID': projectID,
-            'User-Name': localStorage.getItem('user'),
-            'User-Secret': localStorage.getItem('password')
-        }
-        axios.get(
-            'https://api.chatengine.io/chats', 
-            {headers:authObject}
-        )
-        .then((response)=>setChats(response.data))
-    }, [chats])
-
-    useEffect(()=>{
-        const getContacts = async ()=>{
-            try {
-                axios.get(
-                    'https://api.chatengine.io/projects/people/',
-                    { headers: { "Private-Key": projectKey } }
-                )
-                .then((response)=>setContacts(response.data))
-            }catch(error){
-                console.log(error)
-            }
-        }
-        getContacts()
-    }, [contacts])
     
     return (
         <div className="d-flex flex-column" style={{width: '20vw'}}>
             <Tab.Container activeKey= {key} onSelect= {(k)=>setKey(k)}>
-                <Nav justify variant="pills" className="mb-1 mt-2">
+                <Nav justify variant="pills" className="mb-0 mt-2">
                     <Nav.Item>
                         <Nav.Link eventKey={chatsKey}>Chats</Nav.Link>
                     </Nav.Item>
@@ -90,17 +58,3 @@ function ChatList() {
 }
 
 export default ChatList
-
-
-
-
-
-
-
-//
-
-/*return (
-    <div style={{width:'20vw'}} className="d-flex flex-column">
-        
-           
-        )*/
