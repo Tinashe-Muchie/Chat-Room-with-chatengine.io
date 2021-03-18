066ab4796253aeb162f441274340d973cd85e2be
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-03-18 16:12:02 +0200

    Sidebar initialiation

---
+++ chat-app/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div className="d-flex flex-column border" style={{width: '20vw'}}>
+           helo
+        </div>
+    )
+}
+
+export default Sidebar
