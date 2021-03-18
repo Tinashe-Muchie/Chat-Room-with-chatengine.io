5052962a9ec91b5758e332d81e0766a08f7184df
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-03-18 16:13:07 +0200

    chatArea modifications

--- chat-app/src/components/ChatArea/ChatArea.js
+++ chat-app/src/components/ChatArea/ChatArea.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Helmet} from 'react-helmet'
+import {Image} from 'react-bootstrap'
 import ChatList from '../ChatList/ChatList'
 import MessageArea from '../MessageArea/MessageArea'
+import Sidebar from '../Sidebar/Sidebar'
+import {Context} from '../Context/Context'
 
 function ChatArea() {
+
+    const {selectChat} = useContext(Context)
+
     return (
         <div>
             <Helmet>
@@ -11,7 +17,18 @@ function ChatArea() {
            </Helmet>
             <div className="d-flex container" style= {{height: '100vh' }}>
                 <ChatList />
-                <MessageArea />
+                { 
+                    (selectChat) 
+                    ?   <MessageArea />
+                    :   <div className="no-chat-selected mx-3" style={{width: '42vw'}}>
+                            <Image 
+                                src= '/img/pointLeft.png'
+                                className="point-Left mr-2"
+                            /> 
+                            Select a Chat
+                        </div>
+                }
+                <Sidebar />
             </div>
         </div>
     )
