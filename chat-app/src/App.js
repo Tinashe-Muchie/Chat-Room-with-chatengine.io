5eb1fbac00cc698da7dfa98df93c7016a162f578
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-03-18 16:13:27 +0200

    app modifications

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -3,25 +3,28 @@ import {ChatEngine} from 'react-chat-engine'
 import './App.css'
 import Login from './components/Login/Login'
 import ChatArea from './components/ChatArea/ChatArea'
+import GlobalContext from './components/Context/Context'
 
 function App() {
 
   return (
     <div>
-    {
-      (!localStorage.getItem('user'))
-      ? <Login />
-      : <>
-          <ChatEngine 
-            publicKey={'db666265-557a-44c8-92a1-f9261e58cc4e'}
-            userName={localStorage.getItem('user')}
-            userSecret={localStorage.getItem('password')}
-            height='100vh'
-            hideUI={true}
-          />
-          <ChatArea />
-        </>  
-    } 
+      <GlobalContext>
+        {
+          (!localStorage.getItem('user'))
+          ? <Login />
+          : <>
+            <ChatEngine 
+              publicKey={'db666265-557a-44c8-92a1-f9261e58cc4e'}
+              userName={localStorage.getItem('user')}
+              userSecret={localStorage.getItem('password')}
+              height='100vh'
+              hideUI={true}
+            />
+            <ChatArea />
+            </>  
+        } 
+      </GlobalContext>
     </div>    
   );
 }
