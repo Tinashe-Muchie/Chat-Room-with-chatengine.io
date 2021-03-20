371ceb2ee738f80a1bf74ac3415ba3e367809bdd
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-03-20 06:50:08 +0200

    ContactsModal modifications

--- chat-app/src/components/Modals/ContactsModal.js
+++ chat-app/src/components/Modals/ContactsModal.js
@@ -1,17 +1,37 @@
-import React from 'react'
-import {Modal, Form, Button} from 'react-bootstrap'
+import React, {useState, useContext} from 'react'
+import {Modal, Form, Button, Alert} from 'react-bootstrap'
+import axios from 'axios'
+import {Context} from '../Context/Context'
+
+const projectID = 'db666265-557a-44c8-92a1-f9261e58cc4e'
 
 function ContactsModal({handleClose, contacts}) {
+
+    const [username, setUsername] = useState('')
+    const [error, setError] = useState('')
+    const {selectChat} = useContext(Context)
+
+    const authObject = {
+        'Project-ID': projectID,
+        'User-Name': localStorage.getItem('user'),
+        'User-Secret': localStorage.getItem('password')
+    }
     
-    const handleSubmit = (e)=> {
+    const handleSubmit = async (e)=> {
         e.preventDefault()
-        
-        handleClose()
+        try {
+            await axios.post(
+                `https://api.chatengine.io/chats/${selectChat.id}/people/`,
+                { 'username': username },
+                { headers: authObject },    
+            )
+            handleClose()
+        }catch(error){
+            setError('Oops! something went wrong, try again.')
+        }
     }
 
-    const handleCheck = () => {
-
-    }
+    const err = (error) ? true : false
 
     return (
         <div className="modal-wrapper">
@@ -28,8 +48,8 @@ function ContactsModal({handleClose, contacts}) {
                                 <Form.Check 
                                     type="checkbox"
                                     label={contact.username}
-                                    value
-                                    onChange={()=>handleCheck(contact.id)}
+                                    value={contact.username}
+                                    onChange={(e)=>setUsername(e.target.value)}
                                 />
                             </Form.Group>
                         ))
@@ -37,6 +57,11 @@ function ContactsModal({handleClose, contacts}) {
                     <Button type="submit" variant="outline-primary" className="mb-3 float-right">
                         Add
                     </Button>
+                    <div className="mt-3" style={{display:'flex', justifyContent:'center'}}>
+                        {(err) && <Alert variant='dark' style={{color: '#C5C6C7', backgroundColor: '#3500D3'}}>
+                            {error}
+                        </Alert>}
+                    </div>
                 </Form>
             </Modal.Body>        
         </div>
